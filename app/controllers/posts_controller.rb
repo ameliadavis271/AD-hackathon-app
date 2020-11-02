5c23beb03bdbb118abb647e1e5ee68a09a25cbfe
Amelia Davis <ameliadavis271@gmail.com>
2020-11-02 05:42:39 -0800

    Basics created. Index, Show, New, Create and Destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
